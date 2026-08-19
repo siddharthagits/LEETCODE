203085688bde3253c65033d83249dc7b15eb0326
Siddhartha <165638111+siddharthagits@users.noreply.github.com>
2026-08-19 12:43:23 +0530

    Time: 75 ms (96.54%), Space: 22.4 MB (25.18%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        k = 0
+        for i in nums:
+            if k < 2 or i != nums[k - 2]:
+                nums[k] = i
+                k += 1
+            
+        return k
